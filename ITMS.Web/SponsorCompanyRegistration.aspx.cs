47aad75f43f9d3a2c127ce20918049d6b8ebe7dd
Theodhori Dhiamanti <teo_dia@hotmail.com>
2014-03-21 23:29:17 -0400

    Fixed Bug with (existing)Company Registration Page

--- ITMS.Web/SponsorCompanyRegistration.aspx.cs
+++ ITMS.Web/SponsorCompanyRegistration.aspx.cs
@@ -7,29 +7,11 @@ using System.Web.UI.WebControls;
 
 namespace ITMS.Web
 {
-    public partial class SponsorCompanyMainRegistrationPage : System.Web.UI.Page
+    public partial class SponsorCompanyRegistrationPage : System.Web.UI.Page
     {
 
 
-        protected void Button1_Click(object sender, EventArgs e)
-        {
-            Panel3.Visible = true;
-            /*
-            Label1.Text = txtFname.Text;
-            Label2.Text = txtLname.Text;
-            Label3.Text = txtTitle.Text;
-            Label4.Text = txtDept.Text;
-            Label5.Text = txtOrg.Text;
-            Label6.Text = txt1OP.Text;
-            Label7.Text = txt1CP.Text;
-            Label8.Text = txt1Email.Text;
-            Label9.Text = txt1Street.Text;
-            Label10.Text = txt1City.Text;
-            Label11.Text = drpState1.Text;
-            Label12.Text = txt1Zip.Text;
-            */
-
-        }
+       
 
 
         protected void btnManager_Click(object sender, EventArgs e)
