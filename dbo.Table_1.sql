7ae0650515eb620293957d213f896323305d98b0
ProfBest <Prof.Best1@gmail.com>
2014-03-31 22:20:15 -0400

    Revert "Revised table - added to database fileuploaddb under app_data, also revised student upload code by studentupload.aspx error with string command and "private void create""
    This reverts commit ad799918fb1d420054cd6688795a0b2e92fe580c.

--- dbo.Table_1.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[Table] (
-    [Id]         INT           NOT NULL,
-    [Name]       NVARCHAR(MAX) NULL,
-    [Filename]   VARCHAR (MAX) NULL,
-    [Filesize]   INT           NULL,
-    [attachment] VARBINARY(MAX) NULL,
-    PRIMARY KEY CLUSTERED ([Id] ASC)
-);
-
